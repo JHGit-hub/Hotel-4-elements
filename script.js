0e02d45e68138e81d8513b2f2e5172038c77c43c
JHGit-hub <julien.hardy2104@gmail.com>
2025-04-04 14:45:58 +0200

    update - carrousel

--- script.js
+++ script.js
@@ -4,66 +4,6 @@
 ////////////// carrousel
 
 if(window.location.href.includes("index.html")){
-//creation des variables
-
-    let carrouselBtnFeu = document.getElementById("carrouselBtnFeu");
-    let nodeSlideFeu = document.querySelectorAll(".carrousel-slideFeu");
-    let tblSlideFeu = Array.from(nodeSlideFeu);
-
-    let carrouselBtnAir = document.getElementById("carrouselBtnAir");
-    let nodeSlideAir = document.querySelectorAll(".carrousel-slideAir");
-    let tblSlideAir = Array.from(nodeSlideAir);
-
-    let carrouselBtnEau = document.getElementById("carrouselBtnEau");
-    let nodeSlideEau = document.querySelectorAll(".carrousel-slideEau");
-    let tblSlideEau = Array.from(nodeSlideEau);
-
-    let carrouselBtnTerre = document.getElementById("carrouselBtnTerre");
-    let nodeSlideTerre = document.querySelectorAll(".carrousel-slideTerre");
-    let tblSlideTerre = Array.from(nodeSlideTerre);
-
-    let i=0;
-
-// action "click" sur carrousel
-
-    carrouselBtnFeu.addEventListener("click", function(){
-        tblSlideFeu[i].classList.add("hidden");
-        if(i<tblSlideFeu.length-1){
-            i++;
-        } else {
-            i=0;
-        };
-        tblSlideFeu[i].classList.remove("hidden");
-    });
-    carrouselBtnEau.addEventListener("click", function(){
-        tblSlideEau[i].classList.add("hidden");
-        if(i<tblSlideEau.length-1){
-            i++;
-        } else {
-            i=0;
-        };
-        tblSlideEau[i].classList.remove("hidden");
-    });
-    carrouselBtnAir.addEventListener("click", function(){
-        tblSlideAir[i].classList.add("hidden");
-        if(i<tblSlideAir.length-1){
-            i++;
-        } else {
-            i=0;
-        };
-        tblSlideAir[i].classList.remove("hidden");
-    });
-    carrouselBtnTerre.addEventListener("click", function(){
-        tblSlideTerre[i].classList.add("hidden");
-        if(i<tblSlideTerre.length-1){
-            i++;
-        } else {
-            i=0;
-        };
-        tblSlideTerre[i].classList.remove("hidden");
-    });
-
-
 
 ////////////// menu burger
 
@@ -114,25 +54,9 @@ if(window.location.href.includes("index.html")){
 
 ////////////////////////////// JS PAGE HOTEL FEU //////////////////////////////
 
-////////////// carrousel
+
 
 if(window.location.href.includes("hotel-feu.html")){
-    let carrouselBtnFeu = document.getElementById("carrouselBtnFeu");
-    let nodeSlideFeu = document.querySelectorAll(".carrousel-slideFeu");
-    let tblSlideFeu = Array.from(nodeSlideFeu);
-    let i=0;
-
-    carrouselBtnFeu.addEventListener("click", function(){
-        tblSlideFeu[i].classList.add("hidden");
-        if(i<tblSlideFeu.length-1){
-            i++;
-        } else {
-            i=0;
-        };
-        tblSlideFeu[i].classList.remove("hidden");
-    });
-
-
 
 ////////////// menu burger
 
